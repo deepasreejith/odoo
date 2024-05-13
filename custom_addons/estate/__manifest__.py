86b6c08bb3dbc85b8f9f14826f1cea3d54e48f4c
Deepa KH <deepakh2014@gmail.com>
2024-05-13 16:51:06 +0400

    estate propery model and its view created

---
+++ custom_addons/estate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Real Estate',
+    'author': 'Deepa',
+    'summary': 'Custom real estate module',
+    'depends': [],
+    'sequence':-200,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/estate_property_views.xml',
+    ],
+    'assets':{
+
+    },
+
+    'application':True
+}
---
+++ custom_addons/estate/models/estate_property.py
@@ -0,0 +1,25 @@
+from odoo import fields, models
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = " estate property details"
+
+    name = fields.Char(string="Name",required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="PostCode")
+    date_availability = fields.Date(string="Date Availability",default=fields.Date.today())
+    expected_price = fields.Float(string="Expected Price",required=True)
+    selling_price = fields.Float(string="Selling Price", readonly=True)
+    bedrooms = fields.Integer(string="Bedrooms",default=4)
+    living_area = fields.Integer(string="Living Area")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area")
+    garden_orientation = fields.Selection(string='Garden Orientation',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],)
+
+
+
+
+
