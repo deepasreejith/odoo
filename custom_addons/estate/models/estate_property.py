6b0a4ea742a149e445565da6b9b6784ddc2245f3
Deepa KH <deepakh2014@gmail.com>
2024-05-15 12:38:03 +0400

    actions applied

--- custom_addons/estate/models/estate_property.py
+++ custom_addons/estate/models/estate_property.py
@@ -1,31 +1,38 @@
-from odoo import fields, models,api
+from odoo import fields, models, api
+from odoo.exceptions import UserError
+
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = " estate property details"
 
-    name = fields.Char(string="Name",required=True)
+
+    name = fields.Char(string="Name", required=True)
     description = fields.Text(string="Description")
     postcode = fields.Char(string="PostCode")
-    date_availability = fields.Date(string="Date Availability",default=fields.Date.today())
-    expected_price = fields.Float(string="Expected Price",required=True)
-    selling_price = fields.Float(string="Selling Price", readonly=True)
-    bedrooms = fields.Integer(string="Bedrooms",default=4)
+    status = fields.Char(string="Status", default="New")
+    date_availability = fields.Date(string="Date Availability", default=fields.Date.today())
+    expected_price = fields.Float(string="Expected Price", required=True)
+    selling_price = fields.Float(string="Selling Price")
+    bedrooms = fields.Integer(string="Bedrooms", default=4)
     living_area = fields.Integer(string="Living Area(sqm)")
     facades = fields.Integer(string="Facades")
     garage = fields.Boolean(string="Garage")
     garden = fields.Boolean(string="Garden")
     garden_area = fields.Integer(string="Garden Area(sqm)")
     garden_orientation = fields.Selection(string='Garden Orientation',
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],)
-    total_area = fields.Float(string='Total Area(sqm)',readonly=True,compute="_compute_total_area")
-    best_price = fields.Float(string='Best Offer', compute ='compute_best_offer' )
+                                          selection=[('north', 'North'), ('south', 'South'), ('east', 'East'),
+                                                     ('west', 'West')], )
+    total_area = fields.Float(string='Total Area(sqm)', readonly=True, compute="_compute_total_area")
+    best_price = fields.Float(string='Best Offer', compute='compute_best_offer')
 
-    property_type = fields.Many2one('estate.property.type',string='Property Type')
+    property_type = fields.Many2one('estate.property.type', string='Property Type')
     property_tag_ids = fields.Many2many('estate.property.tag')
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
+    salesperson = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user.id)
+    buyer = fields.Char(string='Buyer')
 
-    @api.depends("living_area","garden_area")
+    @api.depends("living_area", "garden_area")
     def _compute_total_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
@@ -46,3 +53,20 @@ class EstateProperty(models.Model):
         else:
             self.garden_area = 0
             self.garden_orientation = ''
+
+    def action_set_cancel(self):
+        if self.status == 'sold':
+            raise UserError("Cannot set sold on cancelled property.")
+        else:
+            self.status = 'cancelled'
+
+    def action_set_sold(self):
+        if self.status == 'cancelled':
+            raise UserError("Cannot set cancelled on sold property.")
+        else:
+            self.status = 'sold'
+
+
+    '''def update_selling_price(self, price):
+        for record in self:
+            record.selling_price = price'''
--- custom_addons/estate/models/estate_property_offer.py
+++ custom_addons/estate/models/estate_property_offer.py
@@ -18,9 +18,22 @@ class EstatePropertyOffer(models.Model):
     @api.depends('create_date','validity')
     def compute_date_deadline(self):
         for record in self:
-            record.date_deadline = record.create_date + timedelta(days=record.validity)
+            if record.create_date:
+                record.date_deadline = fields.Datetime.from_string(record.create_date).date() + timedelta(
+                    days=record.validity)
+
     def inverse_date_deadline(self):
-            for record in self:
-                if record.date_deadline and record.create_date:
-                    create_date = record.create_date.date()
-                    record.validity = (record.date_deadline - create_date).days
+        for record in self:
+            if record.date_deadline and record.create_date:
+                create_date = fields.Datetime.from_string(record.create_date).date()
+                record.validity = (record.date_deadline - create_date).days
+
+    def action_confirm(self):
+        for record in self:
+           # record.property_id.update_selling_price(record.price)
+            record.property_id.selling_price = record.price
+            record.property_id.buyer = record.partner_id.name
+            record.write({'status': 'accepted'})
+
+    def action_cancel(self):
+        self.write({'status': 'refused'})
