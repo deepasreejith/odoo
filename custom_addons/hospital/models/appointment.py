2084b26b3661aee09fe4d9b29dc779d4b9c10100
Deepa KH <deepakh2014@gmail.com>
2024-05-24 10:00:27 +0400

    create appointment model and features

--- custom_addons/hospital/models/appointment.py
+++ custom_addons/hospital/models/appointment.py
@@ -4,8 +4,14 @@ class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
     _description = 'Hospital Appointment'
     _inherit = ['mail.thread','mail.activity.mixin']
+    _rec_name = 'patient_id'
 
     patient_id = fields.Many2one('hospital.patient',string='Patient')
     appointment_time = fields.Datetime(string='Appointment Time',default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date',default=fields.Date.context_today)
     gender = fields.Selection(related='patient_id.gender')
+    ref = fields.Char(string='Reference')
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
