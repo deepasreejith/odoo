bcd1d920a3db7f5b5ff243e511700b377474a398
Deepa KH <deepakh2014@gmail.com>
2024-11-05 12:51:25 +0400

    create basic sippet

---
+++ custom_addons/create_snippet/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Create Snippet',
+    'summary': 'Create snippet',
+    'sequence':-200,
+    'description': """
+        This module create my own snippets
+    """,
+    'author': 'Deepa',
+    'depends': ['website'],
+    'data': [
+        'views/snippet.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
